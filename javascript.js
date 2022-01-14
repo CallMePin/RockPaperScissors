aaa56ee3f8a22707ac914d1e720cc13d588917b0
CallMePin <callmepin@aol.com>
2022-01-14 16:47:49 -0600

    Added function for single round of RPS

--- javascript.js
+++ javascript.js
@@ -1,8 +1,22 @@
 
-
 function computerPlay() {
     var answer = ["Rock", "Paper", "Scissors"];
     return randomAnswer = answer[Math.floor(Math.random() * answer.length)];
 }
 
-console.log(computerPlay());
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
+        return "It's a tie!";
+    } else if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "paper") {
+        return "You lose! Paper beats Rock";
+    } else {
+        return "You win! Rock beats Scissors";
+    }
+}
+
+const playerSelection = "Rock";
+const computerSelection = computerPlay();
+console.log("You've chosen " + playerSelection);
+console.log("The computer has chosen " + computerSelection);
+console.log(playRound(playerSelection, computerSelection));
