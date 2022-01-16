35b21f752970670d8e52b05009af546cfd998729
CallMePin <callmepin@aol.com>
2022-01-16 13:48:12 -0600

    Added 5 rounds for a game

--- javascript.js
+++ javascript.js
@@ -8,17 +8,24 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
         return "It's a tie!";
-    } else if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "paper") {
-        return "You lose! Paper beats Rock";
+    } else if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "paper" || 
+                playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "scissors" ||
+                playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "rock") {
+        return "You lose! " + computerSelection + " beats " + playerSelection;
     } else {
-        return "You win! Rock beats Scissors";
+        return "You win! " + playerSelection + " beats " + computerSelection;
     }
 }
 
-// const playerSelection = "Rock";
-let playerSelection = window.prompt("Please choose Rock, Paper, or Scissors");
-const computerSelection = computerPlay();
-console.log("You've chosen " + playerSelection);
-console.log("The computer has chosen " + computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    for (let i = 1; i <= 5; i++) {
+        let playerSelection = window.prompt("Please choose Rock, Paper, or Scissors");
+        const computerSelection = computerPlay();
+        console.log("Round " + i);
+        console.log("You've chosen " + playerSelection);
+        console.log("The computer has chosen " + computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
 
+game();
